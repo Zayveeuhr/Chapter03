a5c6a269f4d528af14f54ca50ecbb623f120bb7f
daelynx <daelynxwinston@gmail.com>
2025-06-04 07:48:03 -0400

    Add project files.

---
+++ Operators/Program.cs
@@ -0,0 +1,12 @@
+﻿#region Exploring unary operators
+
+int a = 3;
+int b = a++; // Postfix means increment a before assigning it.
+WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c; // Prefix means increment c before assigning it.
+WriteLine($"a is {c}, b is {d}");
+
+
+#endregion
