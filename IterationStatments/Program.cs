4b93aa4201bcae41f31cc9dc4988396e873e8049
daelynx <daelynxwinston@gmail.com>
2025-06-06 13:04:29 -0400

    Added a for  loop for practice.

--- IterationStatments/Program.cs
+++ IterationStatments/Program.cs
@@ -11,24 +11,40 @@ while (x < 10)
 #endregion
 
 #region Looping with the do statement
+// commited out so I do not have ot enter password every time I run the program.
 
-string? actualPassword = "pa$$w0rd";
-string? password;
-int? count = 0;
+//string? actualPassword = "pa$$w0rd";
+//string? password;
+//int? count = 0;
 
-do
+//do
+//{
+//    count++;
+//    if (count == 4)
+//    {
+//        break;
+//    }
+
+//    Write("Enter your password: ");
+//    password = ReadLine();
+
+//} while (password != actualPassword);
+
+//WriteLine(count == 4? "\nError to many attempts": "Correct!");
+
+#endregion
+
+#region Loop with the for loop statement
+
+WriteLine();
+for (int y =1; y <= 10; y ++)
 {
-    count++;
-    if (count == 4)
-    {
-        break;
-    }
+    WriteLine(y);
+}
 
-    Write("Enter your password: ");
-    password = ReadLine();
-
-} while (password != actualPassword);
-
-WriteLine(count == 4? "\nError to many attempts": "Correct!");
+for (int y = 0; y <= 10; y += 3)
+{
+    WriteLine(y);
+}
 
 #endregion
